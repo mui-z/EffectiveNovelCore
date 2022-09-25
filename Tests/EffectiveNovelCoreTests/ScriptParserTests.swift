c25c12f13afb37141500e386f4d1f4b47b66f85a
mui-z <93278577+mh-idea@users.noreply.github.com>
2022-09-25 22:53:57 +0900

    fix: fix test

--- Tests/EffectiveNovelCoreTests/ScriptParserTests.swift
+++ Tests/EffectiveNovelCoreTests/ScriptParserTests.swift
@@ -6,44 +6,45 @@ final class ScriptParserTests: XCTestCase {
     func testParseText() {
         let parser = ScriptParser()
 
-        XCTAssertEqual(try! parser.parse(rawAllString: "ab"), [.character(char: "a"), .character(char: "b")])
+        XCTAssertEqual(try! parser.parse(rawAllString: "ab[e]"), [.character(char: "a"), .character(char: "b"), .end])
 
         let multiLineText = """
                             aa
                             b
+                            [e]
                             """
-        XCTAssertEqual(try! parser.parse(rawAllString: multiLineText), [.character(char: "a"), .character(char: "a"), .character(char: "b")])
+        XCTAssertEqual(try! parser.parse(rawAllString: multiLineText), [.character(char: "a"), .character(char: "a"), .character(char: "b"), .end])
     }
 
     func testParseAllCommands() {
         let parser = ScriptParser()
 
-        XCTAssertEqual(try! parser.parse(rawAllString: "[n]"), [.newline])
-        XCTAssertEqual(try! parser.parse(rawAllString: "[tw]"), [.tapWait])
-        XCTAssertEqual(try! parser.parse(rawAllString: "[twn]"), [.tapWaitAndNewline])
-        XCTAssertEqual(try! parser.parse(rawAllString: "[cl]"), [.clear])
-        XCTAssertEqual(try! parser.parse(rawAllString: "[delay speed=1000]"), [.delay(speed: 1000)])
-        XCTAssertEqual(try! parser.parse(rawAllString: "[resetdelay]"), [.resetDelay])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[n][e]"), [.newline, .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[tw][e]"), [.tapWait, .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[twn][e]"), [.tapWaitAndNewline, .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[cl][e]"), [.clear, .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[delay speed=1000][e]"), [.delay(speed: 1000), .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[resetdelay][e]"), [.resetDelay, .end])
         XCTAssertEqual(try! parser.parse(rawAllString: "[e]"), [.end])
     }
 
     func testParseEvents() {
         let parser = ScriptParser()
 
-        XCTAssertEqual(try! parser.parse(rawAllString: "[cl]"), [.clear])
-        XCTAssertEqual(try! parser.parse(rawAllString: "[cl][n]"), [.clear, .newline])
-        XCTAssertEqual(try! parser.parse(rawAllString: "s[tw]"), [.character(char: "s"), .tapWait])
-        XCTAssertEqual(try! parser.parse(rawAllString: "[cl]e"), [.clear, .character(char: "e")])
-        XCTAssertEqual(try! parser.parse(rawAllString: "s[cl]e"), [.character(char: "s"), .clear, .character(char: "e")])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[cl][e]"), [.clear, .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[cl][n][e]"), [.clear, .newline, .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "s[tw][e]"), [.character(char: "s"), .tapWait, .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[cl]e[e]"), [.clear, .character(char: "e"), .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "s[cl]e[e]"), [.character(char: "s"), .clear, .character(char: "e"), .end])
     }
 
     func testContainValueEvent() {
         let parser = ScriptParser()
 
-        XCTAssertEqual(try! parser.parse(rawAllString: "[delay speed=1000]"), [.delay(speed: 1000)])
-        XCTAssertEqual(try! parser.parse(rawAllString: "[delay speed=1000][delay speed=2000]"), [.delay(speed: 1000), .delay(speed: 2000)])
-        XCTAssertEqual(try! parser.parse(rawAllString: "t[delay speed=1000]"), [.character(char: "t"), .delay(speed: 1000)])
-        XCTAssertEqual(try! parser.parse(rawAllString: "[delay speed=1000]t"), [.delay(speed: 1000), .character(char: "t")])
-        XCTAssertEqual(try! parser.parse(rawAllString: "s[delay speed=1000]e"), [.character(char: "s"), .delay(speed: 1000), .character(char: "e")])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[delay speed=1000][e]"), [.delay(speed: 1000), .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[delay speed=1000][delay speed=2000][e]"), [.delay(speed: 1000), .delay(speed: 2000), .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "t[delay speed=1000][e]"), [.character(char: "t"), .delay(speed: 1000), .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "[delay speed=1000]t[e]"), [.delay(speed: 1000), .character(char: "t"), .end])
+        XCTAssertEqual(try! parser.parse(rawAllString: "s[delay speed=1000]e[e]"), [.character(char: "s"), .delay(speed: 1000), .character(char: "e"), .end])
     }
 }
