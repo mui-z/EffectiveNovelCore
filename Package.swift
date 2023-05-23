d70e0f171c0f35c45152ecbf0a1f5721b96a12a8
mui-z <93278577+mui-z@users.noreply.github.com>
2023-05-24 00:32:23 +0900

    update: update lib version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/hmlongco/Factory", .upToNextMinor(from: "1.2.8"))
+        .package(url: "https://github.com/hmlongco/Factory", .upToNextMinor(from: "2.1.5"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
